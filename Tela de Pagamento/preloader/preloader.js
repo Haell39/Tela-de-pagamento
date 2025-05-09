63e7757ffafc9d4a098c8c9d3d602e4e26cad2b7
Rael <santosrafaelpro@gmail.com>
2025-05-09 09:13:37 -0300

    creating venv and ++

--- Tela de Pagamento/preloader/preloader.js
+++
@@ -1,7 +0,0 @@
-// Aguarda 2 segundos (2000 milissegundos) e redireciona para outra página
-setTimeout(function() {
-    window.location.href = "../cartao-de-credito/pagamento-credito.html";
-    // Caminho relativo
-    // Substitua 'proxima_pagina.html' pela URL desejada
-  }, 2000);
-  
---
+++ preloader/preloader.js
@@ -0,0 +1,7 @@
+// Aguarda 2 segundos (2000 milissegundos) e redireciona para outra página
+setTimeout(function() {
+    window.location.href = "../cartao-de-credito/pagamento-credito.html";
+    // Caminho relativo
+    // Substitua 'proxima_pagina.html' pela URL desejada
+  }, 2000);
+  
